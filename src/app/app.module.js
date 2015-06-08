ad2ede6a3636293d7ba1071f51e07d6f8c17fcbc
cozmy <cosmin92_ab@yahoo.com>
2015-06-08 12:39:44 +0300

    Added the greeter module

---
+++ src/app/app.module.js
@@ -0,0 +1,25 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('HyperionApp', [
+      //////////////////////////////
+      // Official angular modules //
+      //////////////////////////////
+
+      'ngSanitize',
+      'ngTouch',
+
+      ///////////////////////
+      // Third party modules //
+      ///////////////////////
+
+      'ui.router',
+
+      ////////////////////
+      // Common modules //
+      ////////////////////
+
+      'common.greeter'
+    ]);
+})();
---
+++ src/common/greeter/greetings.directive.js
@@ -0,0 +1,25 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('common.greeter', [])
+    .directive('hypGreetings', function () {
+      return {
+        link: function (scope, iElement) {
+          var
+            currentHour = (new Date()).getHours(),
+            greetings = 'Good Evening,';
+
+          if (currentHour > 5 && currentHour < 12) {
+            greetings = 'Good Morning,';
+          } else if (currentHour >= 12 && currentHour < 17) {
+            greetings = 'Good Afternoon,';
+          }
+
+          iElement.text(greetings);
+        },
+        restrict: 'A',
+        scope: {},
+      };
+    });
+})();
