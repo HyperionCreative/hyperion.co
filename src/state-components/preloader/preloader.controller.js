5c0933f61aaf65a41fa1803dc363e3834e82bf8e
cozmy <cosmin92_ab@yahoo.com>
2015-06-08 16:53:29 +0300

    The preloader now uses ngAnimate

--- src/state-components/preloader/preloader.controller.js
+++ src/state-components/preloader/preloader.controller.js
@@ -4,12 +4,21 @@
   angular
     .module('state.state-components')
     .controller('PreloaderCtrl', ['$scope', 'Preloader', function ($scope, Preloader) {
+      $scope.showPreloader = true;
       $scope.preloaderProgress = 0;
 
       $scope.$watch(function () {
         return Preloader.getProgress('fancy-slider');
       }, function (newValue) {
         $scope.preloaderProgress = newValue;
+
+        if (newValue === 100) {
+          // Needed as otherwise angular won't bind preloaderProgress's new value.
+          // This means that the preloader will not fade out at 100%.
+          $scope.$evalAsync(function () {
+            $scope.showPreloader = false;
+          });
+        }
       });
 
       Preloader.start('fancy-slider');
