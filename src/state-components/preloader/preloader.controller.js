c954632fdee9ca2875101244f96880f4a7c1328b
cozmy <cosmin@hyperion.co>
2015-07-02 11:13:18 +0300

    Unregister the watch once the preloader has finished

--- src/state-components/preloader/preloader.controller.js
+++ src/state-components/preloader/preloader.controller.js
@@ -7,17 +7,20 @@
       $scope.showPreloader = true;
       $scope.preloaderProgress = 0;
 
-      $scope.$watch(function () {
+      var preloaderListener = $scope.$watch(function () {
         return Preloader.getProgress('fancy-slider');
       }, function (newValue) {
         $scope.preloaderProgress = newValue;
 
         if (newValue === 100) {
-          // Needed as otherwise angular won't bind preloaderProgress's new value.
-          // This means that the preloader will not fade out at 100%.
+          // This needs to be called async. 
+          // Without this the preloader wouldn't fade out at 100%.
           $scope.$evalAsync(function () {
             $scope.showPreloader = false;
           });
+
+          // Unregisters the (night) watch
+          preloaderListener();
         }
       });
 
