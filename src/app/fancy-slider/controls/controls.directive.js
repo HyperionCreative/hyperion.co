05c532f18a183ed04844aebbb3984357ce62a84a
cozmy <cosmin@hyperion.co>
2015-07-02 14:29:33 +0300

    Added the controls module

---
+++ src/app/fancy-slider/controls/controls.directive.js
@@ -0,0 +1,30 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('app.fancy-slider.controls', [])
+    .directive('hypFancySliderControls', function () {
+      return {
+        link: function (scope) {
+          // todo link this
+          
+          scope.changeLeft = changeLeft;
+          scope.changeRight = changeRight;
+
+          function changeLeft() {
+            console.log('left');
+          }
+
+          function changeRight() {
+            console.log('right');
+          }
+        },
+        replace: false,
+        // todo uncomment this
+        // require: '^^hypFancySlider',
+        restrict: 'E',
+        scope: {},
+        templateUrl: 'app/fancy-slider/controls/controls.html'
+      };
+    });
+})();
--- src/app/fancy-slider/fancy-slider.module.js
+++ src/app/fancy-slider/fancy-slider.module.js
@@ -3,6 +3,7 @@
 
   angular
     .module('specific.fancy-slider', [
+      'app.fancy-slider.controls',
       'app.fancy-slider.resizer'
     ]);
 })();
