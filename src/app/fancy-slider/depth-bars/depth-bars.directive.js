519fb96b10304c5372f9bac9de7ad9518982c10a
cozmy <cosmin@hyperion.co>
2015-07-03 14:44:42 +0300

    Added DepthBars module

---
+++ src/app/fancy-slider/depth-bars/depth-bars.directive.js
@@ -0,0 +1,31 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('specific.fancy-slider.depth-bars', [
+      'common.css-vendor-prefixer'
+    ])
+    .directive('hypFancySliderDepthBars', ['CssVendorPrefixer', 'FancySliderResizer', function (CssVendorPrefixer, FancySliderResizer) {
+      return {
+        link: function (scope, iElement, iAttrs) {
+          // todo shouldn't it add the scaling property to all bars?
+          if (angular.isDefined(iAttrs.original)) {
+            var target = angular.element(iElement[0].querySelectorAll('.bar'));
+
+            FancySliderResizer.onProportionChange(function (proportion) {
+              target.css(CssVendorPrefixer.prefixProperty('transform', 'scale(' + (1 / proportion) + ',' + (1 / proportion) + ')'));
+            });
+          }
+        },
+        replace: true,
+        restrict: 'E',
+        templateUrl: function (tElement, tAttrs) {
+          if (angular.isDefined(tAttrs.blurred)) {
+            return 'app/fancy-slider/depth-bars/blurred.html';
+          }
+
+          return 'app/fancy-slider/depth-bars/original.html';
+        }
+      };
+    }]);
+})();
--- src/app/fancy-slider/fancy-slider.module.js
+++ src/app/fancy-slider/fancy-slider.module.js
@@ -5,6 +5,7 @@
     .module('specific.fancy-slider', [
       'app.fancy-slider.animations',
       'app.fancy-slider.controls',
+      'specific.fancy-slider.depth-bars',
       'app.fancy-slider.resizer'
     ]);
 })();
