1ef267df0d6fea611bb998ae0343f11243496973
cozmy <cosmin@hyperion.co>
2015-07-07 02:48:15 +0300

    Added some of the old slider's functionality

--- src/app/fancy-slider/fancy-slider.directive.js
+++ src/app/fancy-slider/fancy-slider.directive.js
@@ -3,14 +3,35 @@
 
   angular
     .module('app.fancy-slider')
-    .directive('hypFancySlider', function () {
+    .directive('hypFancySlider', ['FancySliderResizer', 'MAX_SUPPORTED_WIDTH', 'MAX_SUPPORTED_HEIGHT', function (FancySliderResizer, MAX_SUPPORTED_WIDTH, MAX_SUPPORTED_HEIGHT) {
       return {
-        link: function () {
-          // todo add functionality?
+        link: function (scope, iElement) {
+          var
+            fancySlider = iElement,
+            fancySliderContainer = angular.element(iElement[0].querySelector('.slides-container'));
+
+          fancySlider.css({
+            'max-height': MAX_SUPPORTED_HEIGHT + 'px',
+            margin: '0 auto',
+            'max-width': MAX_SUPPORTED_WIDTH + 'px'
+          });
+
+          fancySliderContainer.css({
+            height: MAX_SUPPORTED_HEIGHT + 'px',
+            width: MAX_SUPPORTED_WIDTH + 'px'
+          });
+
+          FancySliderResizer.onProportionChange(function (proportion) {
+            fancySliderContainer.css({
+              'margin-left': -(MAX_SUPPORTED_WIDTH * proportion / 2) + 'px',
+              // todo add vendor prefixes
+              'transform': 'scale(' + proportion + ',' + proportion + ')'
+            });
+          });
         },
         replace: true,
         restrict: 'E',
         templateUrl: 'app/fancy-slider/fancy-slider.html'
       };
-    });
+    }]);
 })();
