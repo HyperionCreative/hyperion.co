2a5b4ddbbdf899ec9e0eafd3599d0e8667c4964a
cozmy <cosmin@hyperion.co>
2015-07-07 16:40:51 +0300

    The blur module now calls the onSuccess callback

--- src/app/fancy-slider/blur/blur.service.js
+++ src/app/fancy-slider/blur/blur.service.js
@@ -3,24 +3,32 @@
 
   angular
     .module('app.fancy-slider.blur')
-    .service('BlurService', function () {
+    .service('BlurService', ['$timeout', function ($timeout) {
       this.blurAll = blurAll;
       this.unblurAll = unblurAll;
 
       this.getBlurStatus = getBlurStatus;
 
+      // Important!!!
+      // This is in sync with the values defined inside _base.scss under Fancy Slider Blur section!
+      var ANIMATION_DURATION = 125;
+
       var isBlurred = false;
 
-      function blurAll (transitionDuration, onSuccess) {
+      function blurAll(onSuccess) {
         isBlurred = true;
+
+        $timeout(onSuccess, ANIMATION_DURATION);
       }
 
-      function unblurAll (transitionDuration, onSuccess) {
+      function unblurAll(onSuccess) {
         isBlurred = false;
+
+        $timeout(onSuccess, ANIMATION_DURATION);
       }
 
       function getBlurStatus() {
         return isBlurred;
       }
-    });
+    }]);
 })();
