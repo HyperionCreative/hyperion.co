5ebaa25c821029e8129ec65d073245d6d7851393
cozmy <cosmin@hyperion.co>
2015-07-15 01:00:34 +0300

    Removed the link function
    Classes can be added directly on the directive.

--- src/app/navigation-hamburger/navigation-hamburger.directive.js
+++ src/app/navigation-hamburger/navigation-hamburger.directive.js
@@ -7,9 +7,6 @@
     ])
     .directive('hypNavigationHamburger', function () {
       return {
-        link: function (scope, iElement, iAttrs) {
-          iElement.addClass(iAttrs.styleOption || 'left');
-        },
         replace: true,
         restrict: 'E',
         templateUrl: 'app/navigation-hamburger/navigation-hamburger.html'
