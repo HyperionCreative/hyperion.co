891f1d0ab74d70a5396cafd59eac9e5d56fa5c07
Cosmin <cosmin@hyperion.co>
2015-08-04 14:54:36 +0300

    Added PIXI as an angular constant

---
+++ src/common/pixi/pixi.module.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+
+  /* jshint ignore:start */
+
+  angular
+    .module('common.pixi', [])
+    .constant('PIXI', PIXI);
+
+  /* jshint ignore:end */
+})();
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -36,6 +36,7 @@ module.exports = function (config) {
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/angular-animate/angular-animate.js',
+      'bower_components/pixi/bin/pixi.js',
       // endbower
       'bower_components/angular-mocks/angular-mocks.js',
 
