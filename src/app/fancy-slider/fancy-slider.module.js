1f57e5b8b601b3d53aa74cd5e637be38bae5fc75
Cosmin <cosmin@hyperion.co>
2015-08-05 19:46:18 +0300

    Deleted all the unused files from /fancy-slider
    These will be added back as I rebuild the slider "the pixi way"

--- src/app/fancy-slider/fancy-slider.module.js
+++ src/app/fancy-slider/fancy-slider.module.js
@@ -3,13 +3,6 @@
 
   angular
     .module('app.fancy-slider', [
-      'app.fancy-slider.animations',
-      'app.fancy-slider.blur',
-      'app.fancy-slider.controls',
-      'app.fancy-slider.depth-bars',
-      'app.fancy-slider.resizer',
-      'app.fancy-slider.resources',
-      'app.fancy-slider.slide-description',
-      'app.fancy-slider.slides'
+      'app.fancy-slider.animations'
     ]);
 })();
