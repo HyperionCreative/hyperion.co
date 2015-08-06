94e947c00e161cb6220709a488ceedecd52e96e1
Cosmin <cosmin@hyperion.co>
2015-08-07 01:24:42 +0300

    Fully implemented the depth bars

--- src/app/fancy-slider/configuration.service.js
+++ src/app/fancy-slider/configuration.service.js
@@ -14,6 +14,9 @@
       this.MIN_WIDTH = 1366;
       this.MIN_HEIGHT = 768;
 
+      // Needed by depth bars to stick to the edge of the screen!
+      this.GLOBAL_MIN_WIDTH = 1024;
+
       // Native resolutions
       this.NATIVE_WIDTH = 2560;
       this.NATIVE_HEIGHT = 1440;
--- src/app/fancy-slider/depth-bars/depth-bars.service.js
+++ src/app/fancy-slider/depth-bars/depth-bars.service.js
@@ -2,8 +2,12 @@
   'use strict';
 
   angular
-    .module('app.fancy-slider.depth-bars', [])
-    .service('FancyDepthBars', ['PIXI', 'FancyConfiguration', function (PIXI, Configuration) {
+    .module('app.fancy-slider.depth-bars', [
+      'app.fancy-slider.resizer',
+      'common.pixi',
+      'common.viewport-size'
+    ])
+    .service('FancyDepthBars', ['PIXI', 'FancyConfiguration', 'FancySliderResizer', 'ViewportSize', function (PIXI, Configuration, SliderResizer, ViewportSize) {
       this.get = get;
 
       ///////////////////
@@ -12,16 +16,76 @@
       var COLOR = 0xFFFFFF;
       var LATERAL_BAR_WIDTH = 40;
 
+      ///////////////
+      // Variables //
+      ///////////////
+      var bars;
+
       ////////////
       // Public //
       ////////////
       function get() {
-        var topBar = drawRectangle(COLOR, 0, 0, Configuration.NATIVE_WIDTH, (function () {
+        if (angular.isUndefined(bars)) {
+          bars = init();
+        }
+
+        return bars;
+      }
+
+      /////////////
+      // Private //
+      /////////////
+      function init() {
+        var topBar = drawRectangle(COLOR, Configuration.NATIVE_WIDTH, (function () {
           // Forgive me God for I have sinned!
           return document.querySelector('.header-index-container').clientHeight;
         })());
-        var leftBar = drawRectangle(COLOR, 0, 0, LATERAL_BAR_WIDTH, Configuration.NATIVE_HEIGHT);
-        var rightBar = drawRectangle(COLOR, Configuration.NATIVE_WIDTH - LATERAL_BAR_WIDTH, 0, LATERAL_BAR_WIDTH, Configuration.NATIVE_HEIGHT);
+        var leftBar = drawRectangle(COLOR, LATERAL_BAR_WIDTH, Configuration.NATIVE_HEIGHT);
+        var rightBar = drawRectangle(COLOR, LATERAL_BAR_WIDTH, Configuration.NATIVE_HEIGHT);
+        rightBar.x = Configuration.NATIVE_WIDTH - rightBar.width;
+
+        // This holds the same bars size as otherwise they would get scaled too.
+        SliderResizer.onProportionChange(function (proportion) {
+          // Sets the new scale
+          topBar.scale.y = 1 / proportion;
+          leftBar.scale.x = 1 / proportion;
+          rightBar.scale.x = 1 / proportion;
+
+          // Rounds the position width otherwise the bars may not properly align
+          topBar.width = Math.round(topBar.width);
+          leftBar.width = Math.round(leftBar.width);
+          rightBar.width = Math.round(rightBar.width);
+        });
+
+        ViewportSize.onChange(function (size) {
+          if (size.width >= 1920) {
+            leftBar.visible = true;
+            rightBar.visible = true;
+
+            var proportion = SliderResizer.getProportion();
+
+            var currentStageWidth = Configuration.NATIVE_WIDTH * proportion;
+            var currentScreenSize = size.width < Configuration.GLOBAL_MIN_WIDTH ? Configuration.GLOBAL_MIN_WIDTH : size.width;
+
+            var currentLateralOffset = Math.abs((currentScreenSize - currentStageWidth) / 2);
+
+            if (size.width === 1920) {
+              // Makes the lateral bar 30px wide.
+
+              leftBar.scale.x = leftBar.scale.x / (LATERAL_BAR_WIDTH / 30);
+              rightBar.scale.x = rightBar.scale.x / (LATERAL_BAR_WIDTH / 30);
+
+              leftBar.width = Math.round(leftBar.width);
+              rightBar.width = Math.round(rightBar.width);
+            }
+
+            leftBar.x = Math.floor(currentLateralOffset * (1 / proportion));
+            rightBar.x = Math.ceil((Configuration.NATIVE_WIDTH - rightBar.width) - (currentLateralOffset * (1 / proportion)));
+          } else {
+            leftBar.visible = false;
+            rightBar.visible = false;
+          }
+        });
 
         return {
           top: topBar,
@@ -30,14 +94,14 @@
         };
       }
 
-      /////////////
-      // Private //
-      /////////////
-      function drawRectangle(color, x, y, width, height) {
+      function drawRectangle(color, width, height) {
         var graphics = new PIXI.Graphics();
 
         graphics.beginFill(color);
-        graphics.drawRect(x, y, width, height);
+        graphics.drawRect(0, 0, width, height);
+
+        // var toReturn = new PIXI.Sprite();
+        // toReturn.addChild(graphics)
 
         return graphics;
       }
