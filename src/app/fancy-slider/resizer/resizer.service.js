3a363b969121f3c744c01956f331c17ee076203d
Cosmin <cosmin@hyperion.co>
2015-08-07 01:15:00 +0300

    Added getProportion method to the resizer module

--- src/app/fancy-slider/resizer/resizer.service.js
+++ src/app/fancy-slider/resizer/resizer.service.js
@@ -8,18 +8,24 @@
     // This doesn't support deregistering a callback. I haven't added this functionality
     // as we don't need one.
     .service('FancySliderResizer', ['FancyConfiguration', 'ViewportSize', function (Configuration, ViewportSize) {
+      this.getProportion = getProportion;
       this.onProportionChange = onProportionChange;
 
       ///////////////
       // Variables //
       ///////////////
       var
-        proportion = getProportion(),
+        proportion = computeProportion(),
         registeredCallbacks = [];
 
       ////////////
       // Public //
       ////////////
+      function getProportion() {
+        // This way we know for sure we get the latest proportion value!
+        return computeProportion();
+      }
+
       // When the callbacks get called they do not trigger a new digest cycle!
       function onProportionChange(callback) {
         registeredCallbacks.push(callback);
@@ -30,8 +36,8 @@
       /////////////
       // Private //
       /////////////
-      function getProportion() {
-        function _getProportion(actual, max, min) {
+      function computeProportion() {
+        function _computeProportion(actual, max, min) {
           // Normalize the 'actual' param
           if (actual <= min) {
             actual = min;
@@ -46,14 +52,14 @@
         var height = currentResolution.height;
 
         var
-          widthProportion = _getProportion(width, Configuration.MAX_WIDTH, Configuration.MIN_WIDTH),
-          heightProportion = _getProportion(height, Configuration.MAX_HEIGHT, Configuration.MIN_HEIGHT);
+          widthProportion = _computeProportion(width, Configuration.MAX_WIDTH, Configuration.MIN_WIDTH),
+          heightProportion = _computeProportion(height, Configuration.MAX_HEIGHT, Configuration.MIN_HEIGHT);
 
         return Math.max(widthProportion, heightProportion);
       }
 
       function onViewportSizeChange() {
-        var possibleProportion = getProportion();
+        var possibleProportion = computeProportion();
 
         if (possibleProportion !== proportion) {
           proportion = possibleProportion;
