e537006084e9f27143f06cd6aa61df05e9bcc696
Cosmin <cosmin@hyperion.co>
2015-08-13 16:31:09 +0300

    Added modules description

--- src/app/fancy-slider/fancy-slider.module.js
+++ src/app/fancy-slider/fancy-slider.module.js
@@ -13,6 +13,30 @@
   // 4. controalele sa suporte cursor custom
   // 5. slider description'ul sa se schimbe si el cu slideul
 
+  // Flow'ul lucrurilor
+  // 1. Initializeaza pixi si il append'uieste in templateul HTML
+  // 2. Apeleaza SliderInitializer.init pentru a initializa toate componentele necesare
+  //    2.1 Se incepe cu Resources
+  //        2.1.1 Imaginile resurselor sunt descarcate (folosindu-ne de AssetsDownloader)
+  //        2.1.2 Resursele sunt create cu imaginile tocmai descarcate
+  //        2.1.3 Acestora le este atribuit comportamentul custom in functie de rezolutie
+  //    2.2 Se continua cu DepthBars
+  //        2.2.1 Imaginile necesare pentru blur sunt descarcate (folosindu-ne de AssetsDownloader)
+  //        2.2.2 Resursele sunt create (cele normale dar si blurSprites)
+  //        2.2.3 Li se atribuie comportamentul custom in functie de rezolutie
+  //    2.3 Se continua cu Blur
+  //        2.3.1 Imaginile necesare pentru blur sunt descarcate (folosindu-ne de AssetsDownloader)
+  //        2.3.2 Creaza resursele necesare dar si preia cele deja existente (blurSprites din DepthBars)
+  //    2.4 Se continua cu Animations
+  //        2.4.1 Animatiile sunt create folosindu'ne de Resources
+  // 3. Sunt introduse in scena DepthBar'urile
+  //    3.1 Tot aici se leaga si stage renderer pentru fiecare Viewport Resize.
+  // 4. Se adauga Resursele
+  // 5. Se adauga Blur'ul
+  // 6. Se aplica z-index'ul
+  // 7. Se leaga Animatiile (rudimental - trebuie refacut)
+  // 8. Se leaga blur'ul (rudimental - trebuie refacut)
+
   angular
     .module('app.fancy-slider', [
       'app.fancy-slider.animations',
--- src/app/fancy-slider/initializer.service.js
+++ src/app/fancy-slider/initializer.service.js
@@ -1,6 +1,12 @@
 (function () {
   'use strict';
 
+  // This is the starting point of the fancy slider.
+  // 
+  // It makes sure everything is up and running before we start the slider's "boot" process.
+  // All of his "init modules" are required to offer an init function which must return a promise.
+  // When the promise is resolved, then that module is considered initialized.
+
   angular
     .module('app.fancy-slider')
     .service('FancySliderInitializer', ['$q', 'FancyAnimations', 'FancyBlur', 'FancyDepthBars', 'FancyResources', function ($q, Animations, Blur, DepthBars, Resources) {
