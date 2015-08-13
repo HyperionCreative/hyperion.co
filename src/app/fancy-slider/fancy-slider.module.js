0bb37c75d43efe4f7a1e3e147628be0706bec2aa
Cosmin <cosmin@hyperion.co>
2015-08-13 14:46:36 +0300

    Added a todo list

--- src/app/fancy-slider/fancy-slider.module.js
+++ src/app/fancy-slider/fancy-slider.module.js
@@ -2,8 +2,16 @@
   'use strict';
 
   // todo
+  // compatibility:
   // 1. FF pe OSX trebuie fara RAF si fara AA 
   // 2. IE 11 (si lower presupun) pe Windows (asta era 7) trebuie cu RAF si AA disabled si fps 30
+  //
+  // tasks:
+  // 1. preloaderul sa stea mai mult ca sa apara slideul full incarcat. acum barele clipesc.
+  // 2. daca dai sa schimbi slideul in timp ce se schimba, sa bage viteza
+  // 3. blurul o sa si schimbe image on resize. acum e fixa
+  // 4. controalele sa suporte cursor custom
+  // 5. slider description'ul sa se schimbe si el cu slideul
 
   angular
     .module('app.fancy-slider', [
