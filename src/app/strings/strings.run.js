6d37774cb7ca6b0c10596ab7d6ce10462adaaf8d
cozmy <cosmin@hyperion.co>
2015-08-24 23:03:21 +0300

    Added portfolio main page to Strings

--- src/app/strings/strings.run.js
+++ src/app/strings/strings.run.js
@@ -14,6 +14,7 @@
           dribbbleHref: '//dribbble.com/hyperion',
           behanceHref: '//www.behance.net/hyperioncreative'
         },
+
         contact: {
           navigationHamburgerAnchorText: 'Contact',
           header: {
@@ -21,6 +22,7 @@
             description: 'If you have any questions or would like to talk to us about a project, please either call or use the form below - we will get back to you within 24h.'
           }
         },
+
         expertise: {
           navigationHamburgerAnchorText: 'Expertise',
           header: {
@@ -38,6 +40,7 @@
             description: '<p>This is perhaps the most valuable aspect of working with any creative agency - having a team of experts in the creative fields to look at things from a different angle and bring insightful ideas to the table. From picking the best suited technologies, to helping structure your product correctly, we\'re here to lend our expertise and bring your idea a little closer to success.</p>'
           }
         },
+
         ourProcesses: {
           navigationHamburgerAnchorText: ' Our Processes',
           header: {
@@ -47,6 +50,14 @@
           firstColumn: '<p><strong>Product Design. </strong>Building a successful app or site requires more than a great, well timed idea - it requires careful planning and execution. We\'re here to help with the latter.</p><p>We understand each project is unique so instead of rushing to the nearest template store, we are going to spend some time understanding who you are, what it is you are trying to do and who is the target market for your product. We will then ink out a basic structure for the product start planning its execution. From a front-end perspective, this usually involves defining the structure for each section or page, creating the designs and coding the interface up. As far as the back end is concerned, we will establish the logic early on, and develop the platform modularly.</p>',
           secondColumn: '<p><strong>Redesign Projects. </strong>In order to create something that actually works, we need to understand not only the issues your company is trying to eliminate, but also the vision it was built on and your objectives. Once we understand that, we will be well positioned to start planning: how to overcome your company\'s pain points and create a site (or app) that will serve your goals. </p><p>After a plan is agreed on, we will get to the actual work, starting with the design stage - where we explore solutions and visual styles and define what the final product will look and feel like. We conclude the project with development, testing and finally, integration or deployment as is the case.</p>',
           thirdColumn: '<p><strong>Consultation. </strong>For consulting projects we\'re lending our ideas and expertise to companies in need of either fresh, objective ideas or a professional\'s opinion. Like product design or redesign projects, we will start by understanding the whole picture and what it is you are trying to achieve. We will then put our minds to work and issue a report with our assessment of the situation and our recommendations. We do not charge for this service.</p>'
+        },
+
+        portfolio: {
+          navigationHamburgerAnchorText: 'Our Work',
+          header: {
+            title: 'Our Work',
+            description: 'These are some of our most recent projects, that we can talk about. Click on each preview to learn more about the work we did.'
+          }
         }
       };
     }]);
