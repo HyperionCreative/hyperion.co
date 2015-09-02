cd0c7347482b2ec0d54f82cba5d69b3dd3a88b5d
cozmy <cosmin@hyperion.co>
2015-09-02 06:33:34 +0300

    Added outdated-browser

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -37,6 +37,7 @@ module.exports = function (config) {
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/json3/lib/json3.js',
+      'bower_components/outdated-browser/outdatedbrowser/outdatedbrowser.min.js',
       'bower_components/pixi/bin/pixi.js',
       // endbower
       'bower_components/angular-mocks/angular-mocks.js',
