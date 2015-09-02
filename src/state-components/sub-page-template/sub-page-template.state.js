f22cfe495e69b08279c1120196d8d93446090592
cozmy <cosmin@hyperion.co>
2015-09-02 03:30:24 +0300

    The sub pages are now navigated using the appropriate direction

--- src/state-components/sub-page-template/sub-page-template.state.js
+++ src/state-components/sub-page-template/sub-page-template.state.js
@@ -9,6 +9,28 @@
           abstract: true,
           views: {
             'page-content@': {
+              controller: ['$rootScope', '$scope', function ($rootScope, $scope) {
+                // Don't forget to change navigation-hamburger.html after you modify the items' order!
+                var navigationStates = [
+                  'root.sub-page-template.expertise',
+                  'root.sub-page-template.our-processes',
+                  'root.sub-page-template.portfolio',
+                  'root.sub-page-template.contact'
+                ];
+
+                $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState) {
+                  var toStateIndex = navigationStates.indexOf(toState.name);
+                  var fromStateIndex = navigationStates.indexOf(fromState.name);
+
+                  var order = toStateIndex - fromStateIndex;
+
+                  if (order > 0) {
+                    $scope.direction = 'right';
+                  } else {
+                    $scope.direction = 'left';
+                  }
+                });
+              }],
               templateUrl: 'state-components/sub-page-template/sub-page-template.html'
             }
           }
