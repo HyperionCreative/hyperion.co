c91041c033f85be5adb17eea8933288fcaa713f5
cozmy <cosmin@hyperion.co>
2015-09-10 17:00:08 +0300

    Preload all of the remaining images

--- src/state-components/preloader/preloader.controller.js
+++ src/state-components/preloader/preloader.controller.js
@@ -8,6 +8,38 @@
       $scope.preloaderProgress = 0;
 
       $rootScope.$on('fancy-slider.ready', function () {
+        // Load the rest of the files
+        Preloader.addToQueue('the-rest-of-the-images', 'assets/images/contact/invalid-icon.png');
+
+        Preloader.addToQueue('the-rest-of-the-images', 'assets/images/expertise/consulting.jpg');
+        Preloader.addToQueue('the-rest-of-the-images', 'assets/images/expertise/ui-ux.jpg');
+
+        Preloader.addToQueue('the-rest-of-the-images', 'assets/images/portfolio/anyvan/logo.png');
+        Preloader.addToQueue('the-rest-of-the-images', 'assets/images/portfolio/anyvan/iphone-frame.png');
+        Preloader.addToQueue('the-rest-of-the-images', 'assets/images/portfolio/anyvan/slide-1.jpg');
+        Preloader.addToQueue('the-rest-of-the-images', 'assets/images/portfolio/anyvan/slide-2.jpg');
+        Preloader.addToQueue('the-rest-of-the-images', 'assets/images/portfolio/anyvan/slide-3.jpg');
+        Preloader.addToQueue('the-rest-of-the-images', 'assets/images/portfolio/anyvan/slide-4.jpg');
+
+        Preloader.addToQueue('the-rest-of-the-images', 'assets/images/portfolio/grow/logo.png');
+        Preloader.addToQueue('the-rest-of-the-images', 'assets/images/portfolio/grow/ipad-and-iphone-frame.png');
+        Preloader.addToQueue('the-rest-of-the-images', 'assets/images/portfolio/grow/slide-1.jpg');
+        Preloader.addToQueue('the-rest-of-the-images', 'assets/images/portfolio/grow/slide-2.jpg');
+
+        Preloader.addToQueue('the-rest-of-the-images', 'assets/images/portfolio/kartist/logo.png');
+        Preloader.addToQueue('the-rest-of-the-images', 'assets/images/portfolio/kartist/imac-frame.png');
+        Preloader.addToQueue('the-rest-of-the-images', 'assets/images/portfolio/kartist/in-page-1.jpg');
+        Preloader.addToQueue('the-rest-of-the-images', 'assets/images/portfolio/kartist/in-page-3.jpg');
+        Preloader.addToQueue('the-rest-of-the-images', 'assets/images/portfolio/kartist/in-page-4.jpg');
+        Preloader.addToQueue('the-rest-of-the-images', 'assets/images/portfolio/kartist/slide-1.jpg');
+        Preloader.addToQueue('the-rest-of-the-images', 'assets/images/portfolio/kartist/slide-2.jpg');
+        Preloader.addToQueue('the-rest-of-the-images', 'assets/images/portfolio/kartist/slide-3.jpg');
+
+        Preloader.addToQueue('the-rest-of-the-images', 'assets/images/portfolio/quizkick/logo.png');
+        Preloader.addToQueue('the-rest-of-the-images', 'assets/images/portfolio/quizkick/1.jpg');
+
+        Preloader.start('the-rest-of-the-images');
+
         $scope.showPreloader = false;
       });
 
