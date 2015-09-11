b3a73795b755cf8afcf2c55d45ba156b7deeb882
cozmy <cosmin@hyperion.co>
2015-09-11 16:01:46 +0300

    Special behaviour for the back arrow on project's page

--- src/app/portfolio-header/portfolio-header.directive.js
+++ src/app/portfolio-header/portfolio-header.directive.js
@@ -1,10 +1,29 @@
 (function () {
   'use strict';
 
+  var projectPageDetector = 'root.sub-page-template.portfolio.';
+  var goToIndex = false;
+
   angular
     .module('app.portfolio-header', [])
+    .run(['$rootScope', function ($rootScope) {
+      $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState) {
+        if (goToIndex && toState.name.indexOf(projectPageDetector) === 0 && fromState.name.indexOf(projectPageDetector) === 0) {
+          // index -> project -> project -> project
+          // No need to anything, carry on!
+        } else if (!goToIndex && fromState.name === 'root.index' && toState.name.indexOf(projectPageDetector) === 0) {
+          // index -> project
+          goToIndex = true;
+        } else {
+          goToIndex = false;
+        }
+      });
+    }])
     .directive('hypPortfolioHeader', function () {
       return {
+        link: function (scope) {
+          scope.goToIndex = goToIndex;
+        },
         scope: {
           description: '@',
           clientLogoUrl: '@',
