9b41fa615b9423b1b1d98e7aa2c85486b6d0b955
cozmy <cosmin@hyperion.co>
2015-09-17 19:49:56 +0300

    Organized index.html and enabled htmlmin
    This way index.html will get minified!

--- Gruntfile.js
+++ Gruntfile.js
@@ -322,16 +322,22 @@ module.exports = function (grunt) {
     htmlmin: {
       dist: {
         options: {
-          collapseWhitespace: true,
-          conservativeCollapse: true,
-          collapseBooleanAttributes: true,
           removeCommentsFromCDATA: true,
-          removeOptionalTags: true
+          removeOptionalTags: true,
+
+          collapseBooleanAttributes: true,
+          collapseWhitespace: true,
+          removeAttributeQuotes: true,
+          removeComments: true,
+          removeEmptyAttributes: true,
+          removeRedundantAttributes: true,
+          removeScriptTypeAttributes: true,
+          removeStyleLinkTypeAttributes: true
         },
         files: [{
           expand: true,
           cwd: '<%= yeoman.dist %>',
-          src: ['**/*.html'],
+          src: ['index.html'],
           dest: '<%= yeoman.dist %>'
         }]
       }
@@ -548,7 +554,7 @@ module.exports = function (grunt) {
     'uglify',
     // 'filerev',
     'usemin',
-    // 'htmlmin'
+    'htmlmin'
   ]);
 
   grunt.registerTask('default', [
