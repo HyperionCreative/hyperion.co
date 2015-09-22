188554ca3c23793ac03617427317193c9adcf4fa
cozmy <cosmin@hyperion.co>
2015-09-22 23:29:08 +0300

    Rearranged kartist's states
    This way they have the default fromProjectToProject animation.

--- src/state-components/portfolio/kartist/kartist.state.js
+++ src/state-components/portfolio/kartist/kartist.state.js
@@ -2,33 +2,28 @@
   'use strict';
 
   // When a user enters on /kartist he will be redirected here.
-  var defaultStateName = 'root.sub-page-template.portfolio.kartist.design';
+  var defaultStateName = 'root.sub-page-template.portfolio.kartist-design';
 
   angular
     .module('state.portfolio')
     .config(['$stateProvider', function ($stateProvider) {
       $stateProvider
         .state('root.sub-page-template.portfolio.kartist', {
-          url: '/kartist',
+          url: '/kartist'
+        })
+        .state('root.sub-page-template.portfolio.kartist-design', {
+          url: '/kartist/design',
           views: {
             'sub-page-content@root.sub-page-template': {
-              templateUrl: 'state-components/portfolio/kartist/kartist.html'
+              templateUrl: 'state-components/portfolio/kartist/kartist-design.html'
             }
           }
         })
-        .state('root.sub-page-template.portfolio.kartist.design', {
-          url: '/design',
+        .state('root.sub-page-template.portfolio.kartist-development', {
+          url: '/kartist/development',
           views: {
-            'kartist-category': {
-              templateUrl: 'state-components/portfolio/kartist/design.html'
-            }
-          }
-        })
-        .state('root.sub-page-template.portfolio.kartist.development', {
-          url: '/development',
-          views: {
-            'kartist-category': {
-              templateUrl: 'state-components/portfolio/kartist/development.html'
+            'sub-page-content@root.sub-page-template': {
+              templateUrl: 'state-components/portfolio/kartist/kartist-development.html'
             }
           }
         });
