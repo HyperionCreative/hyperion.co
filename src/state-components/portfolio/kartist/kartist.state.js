00f9f29e069d1dc4faadd38dfb3f6d7bde5d979e
cozmy <cosmin@hyperion.co>
2015-09-22 19:41:27 +0300

    Separated kartist into 2 categories: design and development

--- src/state-components/portfolio/kartist/kartist.state.js
+++ src/state-components/portfolio/kartist/kartist.state.js
@@ -1,5 +1,9 @@
 (function () {
   'use strict';
+
+  // When a user enters on /kartist he will be redirected here.
+  var defaultStateName = 'root.sub-page-template.portfolio.kartist.design';
+
   angular
     .module('state.portfolio')
     .config(['$stateProvider', function ($stateProvider) {
@@ -10,6 +14,32 @@
             'sub-page-content@root.sub-page-template': {
               templateUrl: 'state-components/portfolio/kartist/kartist.html'
             }
+          }
+        })
+        .state('root.sub-page-template.portfolio.kartist.design', {
+          url: '/design',
+          views: {
+            'kartist-category': {
+              templateUrl: 'state-components/portfolio/kartist/design.html'
+            }
+          }
+        })
+        .state('root.sub-page-template.portfolio.kartist.development', {
+          url: '/development',
+          views: {
+            'kartist-category': {
+              templateUrl: 'state-components/portfolio/kartist/development.html'
+            }
+          }
+        });
+    }])
+    .run(['$rootScope', '$state', function ($rootScope, $state) {
+      $rootScope.$on('$stateChangeStart',
+        function (event, toState) {
+          if (toState.name === 'root.sub-page-template.portfolio.kartist') {
+            event.preventDefault();
+
+            $state.go(defaultStateName);
           }
         });
     }]);
