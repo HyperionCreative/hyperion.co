846098c13bcc87e9b9dd084ad3b0633a9b3f8f0d
cozmy <cosmin@hyperion.co>
2015-09-28 13:21:47 +0300

    Simple footer now allows custom texts

--- src/app/simple-footer/simple-footer.directive.js
+++ src/app/simple-footer/simple-footer.directive.js
@@ -3,11 +3,24 @@
 
   angular
     .module('app.simple-footer', [])
-    .directive('hypSimpleFooter', function () {
+    .directive('hypSimpleFooter', ['$rootScope', function ($rootScope) {
       return {
+        link: function (scope) {
+          // As we're in a prive scope, we need to copy the "Strings" defined
+          // in the $rootScope.
+          scope.str = $rootScope.str;
+
+          scope.customFooterText = false;
+          if (angular.isString(scope.footerText) && scope.footerText.length > 0) {
+            scope.customFooterText = true;
+          }
+        },
         replace: true,
         restrict: 'E',
+        scope: {
+          footerText: '=?'
+        },
         templateUrl: 'app/simple-footer/simple-footer.html'
       };
-    });
+    }]);
 })();
