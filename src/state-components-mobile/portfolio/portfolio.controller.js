907386459b4cba221a728616904079823b76d8c6
cozmy <cosmin@hyperion.co>
2015-10-02 15:00:46 +0300

    (mobile) Finished the mobile portfolio template

--- src/state-components-mobile/portfolio/portfolio.controller.js
+++ src/state-components-mobile/portfolio/portfolio.controller.js
@@ -8,23 +8,40 @@
       // The href is the state name.
       var navigationOrder = [{
         href: 'portfolio.kartist',
+        imageSrc: 'assets/images/portfolio/kartist/logo.png',
         name: 'Kartist'
       }, {
         href: 'portfolio.quizkick',
+        imageSrc: 'assets/images/portfolio/quizkick/logo.png',
         name: 'QuizKick'
       }, {
         href: 'portfolio.anyvan',
+        imageSrc: 'assets/images/portfolio/anyvan/logo.png',
         name: 'AnyVan'
       }, {
         href: 'portfolio.grow',
+        imageSrc: 'assets/images/portfolio/grow/logo.png',
         name: 'Grow with a CFO'
       }];
 
       $scope.getStateUrl = $state.href;
 
+      $scope.portfolioRsiOptions = {
+        autoScaleSlider: true,
+        autoScaleSliderWidth: 587,
+        autoScaleSliderHeight: 391,
+        navigateByClick: false,
+        slidesSpacing: 0,
+        visibleNearby: {
+          enabled: false
+        }
+      };
+
       $scope.$on('$stateChangeSuccess', function (event, toState) {
         for (var i = 0; i < navigationOrder.length; i++) {
           if (navigationOrder[i].href === toState.name) {
+            $scope.current = navigationOrder[i];
+
             $scope.prev = navigationOrder[i - 1 < 0 ? navigationOrder.length - 1 : i - 1];
             $scope.next = navigationOrder[(i + 1) % navigationOrder.length];
 
