d17b46711116258f87d45d3969cb91370fa9e4b7
cozmy <cosmin@hyperion.co>
2015-10-06 14:51:09 +0300

    (bug fix) GA wasn't working properly on some devices

--- src/common/google-analytics/google-analytics.run.js
+++ src/common/google-analytics/google-analytics.run.js
@@ -1,17 +1,39 @@
 (function () {
   'use strict';
 
-  var ga = window.ga;
 
   angular
     .module('common.google-analytics', [])
-    .run(['$location', '$rootScope', function ($location, $rootScope) {
-      ga('create', 'UA-67881622-1', 'auto');
+    .run(['$location', '$rootScope', '$timeout', function ($location, $rootScope, $timeout) {
+      ///////////////
+      // Run block //
+      ///////////////
+      $timeout(function () {
+        tryToConnect(8, 250);
+      }, 250);
 
-      $rootScope.$on('$stateChangeSuccess', function () {
-        ga('send', 'pageview', {
-          page: $location.absUrl()
+      ///////////////
+      // Functions //
+      ///////////////
+      function tryToConnect(retryCount, timeToSleep) {
+        if (angular.isFunction(window.ga)) {
+          connectToGA(window.ga);
+        } else if (retryCount > 0) {
+          setTimeout(function () {
+            tryToConnect(retryCount - 1, timeToSleep);
+          }, timeToSleep);
+        }
+      }
+
+      function connectToGA(ga) {
+        ga('create', 'UA-67881622-1', 'auto');
+        ga('send', 'pageview');
+
+        $rootScope.$on('$stateChangeSuccess', function () {
+          ga('send', 'pageview', {
+            page: $location.absUrl()
+          });
         });
-      });
+      }
     }]);
 })();
