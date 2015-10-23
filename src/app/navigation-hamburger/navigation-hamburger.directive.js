482bcc87df315d3b2e7f1d7128cff64c1aef3d65
cozmy <cosmin@hyperion.co>
2015-10-23 14:52:20 +0300

    Added the option to always show links for hyp-navigation-hamburger

--- src/app/navigation-hamburger/navigation-hamburger.directive.js
+++ src/app/navigation-hamburger/navigation-hamburger.directive.js
@@ -5,7 +5,7 @@
     .module('app.navigation-hamburger', [
       'common.simple-hoverable'
     ])
-    .directive('hypNavigationHamburger', ['$rootScope', '$state', function ($rootScope, $state) {
+    .directive('hypNavigationHamburger', ['$compile', '$rootScope', '$state', function ($compile, $rootScope, $state) {
       var currentState = {
         name: $state.current.name
       };
@@ -15,8 +15,18 @@
       });
 
       return {
-        link: function (scope) {
+        link: function (scope, iElement, iAttrs) {
           scope.currentState = currentState;
+
+          var menuContainer = angular.element(iElement[0].querySelector('.menu-container'));
+          if (angular.isUndefined(iAttrs.alwaysShowLinks)) {
+            menuContainer.attr('hyp-simple-hoverable', '');
+            menuContainer.attr('on-mouseenter', '.icon-three-horizontal-bars');
+
+            $compile(menuContainer)(scope);
+          } else {
+            menuContainer.addClass('mouse-entered');
+          }
         },
         replace: true,
         restrict: 'E',
