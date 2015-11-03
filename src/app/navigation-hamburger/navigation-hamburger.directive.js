28b8361382dedc7dea96d5b4a25fb8b86e4bbac1
cozmy <cosmin@hyperion.co>
2015-11-03 13:18:46 +0200

    (desktop) Removed "home" from the main nav

--- src/app/navigation-hamburger/navigation-hamburger.directive.js
+++ src/app/navigation-hamburger/navigation-hamburger.directive.js
@@ -5,20 +5,11 @@
     .module('app.navigation-hamburger', [
       'common.simple-hoverable'
     ])
-    .directive('hypNavigationHamburger', ['$compile', '$rootScope', '$state', function ($compile, $rootScope, $state) {
-      var currentState = {
-        name: $state.current.name
-      };
-
-      $rootScope.$on('$stateChangeSuccess', function (event, toState) {
-        currentState.name = toState.name;
-      });
-
+    .directive('hypNavigationHamburger', ['$compile', function ($compile) {
       return {
         link: function (scope, iElement, iAttrs) {
-          scope.currentState = currentState;
-
           var menuContainer = angular.element(iElement[0].querySelector('.menu-container'));
+          
           if (angular.isUndefined(iAttrs.alwaysShowLinks)) {
             menuContainer.attr('hyp-simple-hoverable', '');
             menuContainer.attr('on-mouseenter', '.icon-three-horizontal-bars');
