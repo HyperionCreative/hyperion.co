f07ecab7f71d5abfca26d68eec6c91b11ff46404
cozmy <cosmin@hyperion.co>
2016-03-10 13:52:00 +0200

    Updated the kartist portfolio page to contain the new tabing system

--- src/state-components/portfolio/kartist/kartist.state.js
+++ src/state-components/portfolio/kartist/kartist.state.js
@@ -1,46 +1,19 @@
 (function () {
   'use strict';
 
-  // When a user enters on /kartist he will be redirected here.
-  var defaultStateName = 'root.sub-page-template.portfolio.kartist-design';
-
   angular
     .module('state.portfolio')
     .config(['$stateProvider', function ($stateProvider) {
       $stateProvider
         .state('root.sub-page-template.portfolio.kartist', {
-          url: '/kartist'
-        })
-        .state('root.sub-page-template.portfolio.kartist-design', {
           data: {
-            pageTitle: 'Kartist Design | Hyperion'
+            pageTitle: 'Kartist | Hyperion'
           },
-          url: '/kartist/design',
+          url: '/kartist',
           views: {
             'sub-page-content@root.sub-page-template': {
-              templateUrl: 'state-components/portfolio/kartist/kartist-design.html'
+              templateUrl: 'state-components/portfolio/kartist/kartist.html'
             }
-          }
-        })
-        .state('root.sub-page-template.portfolio.kartist-development', {
-          data: {
-            pageTitle: 'Kartist Development | Hyperion'
-          },
-          url: '/kartist/development',
-          views: {
-            'sub-page-content@root.sub-page-template': {
-              templateUrl: 'state-components/portfolio/kartist/kartist-development.html'
-            }
-          }
-        });
-    }])
-    .run(['$rootScope', '$state', function ($rootScope, $state) {
-      $rootScope.$on('$stateChangeStart',
-        function (event, toState) {
-          if (toState.name === 'root.sub-page-template.portfolio.kartist') {
-            event.preventDefault();
-
-            $state.go(defaultStateName);
           }
         });
     }]);
