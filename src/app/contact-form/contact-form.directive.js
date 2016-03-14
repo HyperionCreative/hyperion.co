cc3218d9c5af4c4509c62064853564415be237e5
cozmy <cosmin@hyperion.co>
2016-03-14 10:18:37 +0200

    Now the contact form submits on enter (on the whole page)

--- src/app/contact-form/contact-form.directive.js
+++ src/app/contact-form/contact-form.directive.js
@@ -3,17 +3,37 @@
 
   angular
     .module('app.contact-form', [])
-    .directive('hypContactForm', function () {
+    .directive('hypContactForm', ['$window',function ($window) {
       return {
-        link: function (scope) {
+        link: function (scope, iElement) {
           scope.isSubmitted = false;
           scope.tryToSubmit = tryToSubmit;
 
           ///////////////
+          // Run block //
+          ///////////////
+          function onEnter(event) {
+            if (event.keyCode === 13 && (event.target && event.target.tagName && event.target.tagName.toLowerCase() !== 'textarea')) {
+              scope.$evalAsync(function(){
+                tryToSubmit();
+              });
+            }
+          }
+
+          angular.element($window).on('keydown', onEnter);
+
+          iElement.on('$destroy', function () {
+            angular.element($window).off('keydown', onEnter);
+          });
+
+          ///////////////
           // Functions //
           ///////////////
-          function tryToSubmit(dataToSubmit, formCtrl) {
-            if (formCtrl.$valid) {
+          function tryToSubmit() {
+            var dataToSubmit = scope.contactForm;
+            scope.contactFormCtrl.$setSubmitted();
+
+            if (scope.contactFormCtrl.$valid) {
               scope.isSubmitted = true;
 
               scope.onSubmit({
@@ -29,5 +49,5 @@
         },
         templateUrl: 'app/contact-form/contact-form.html'
       };
-    });
+    }]);
 })();
