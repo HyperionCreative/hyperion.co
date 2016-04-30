b8d5e3583274173aac596fad0629292e9fc78372
cozmy <cosmin@hyperion.co>
2016-04-30 13:50:57 +0300

    Excluded $budget and $company from email-sender.php
    I've also refactored some of the code.

--- src/php/email-sender.php
+++ src/php/email-sender.php
@@ -1,61 +1,72 @@
 <?php
-  $json = file_get_contents('php://input');
-  $obj = json_decode($json);
+$json_message = json_decode(file_get_contents('php://input'));
 
-  /////////////////////
-  // Parse form data //
-  /////////////////////
-  $name; $help; $contactBack; $budget; $company;
+/////////////////////
+// Parse form data //
+/////////////////////
+$name = isset($json_message->formData->name) ? $json_message->formData->name : '[no name]';
+$help = isset($json_message->formData->help) ? $json_message->formData->help : '[no message]';
+$contactBack = isset($json_message->formData->contactBack) ? $json_message->formData->contactBack : '[no contact method]';
 
-  if (isset($obj->formData->name)) { $name = $obj->formData->name; } else { $name = '[no name]'; };
-  if (isset($obj->formData->help)) { $help = $obj->formData->help; } else { $help = '[no message]'; };
-  if (isset($obj->formData->contactBack)) { $contactBack = $obj->formData->contactBack; } else { $contactBack = '[no contact method]'; };
-  if (isset($obj->formData->budget)) { $budget = $obj->formData->budget; } else { $budget = '[unspecified]'; };
-  if (isset($obj->formData->company)) { $company = $obj->formData->company; } else { $company = '[unspecified]'; };
+///////////////////////////
+// Parse navigation data //
+///////////////////////////
+$referrer = isset($json_message->navigationHistory->referrer) ? $json_message->navigationHistory->referrer : '[unknown]';
+$statesName = (isset($json_message->navigationHistory->statesName) && is_array($json_message->navigationHistory->statesName)) ? $json_message->navigationHistory->statesName : array('[unknown]');
 
-  ///////////////////////////
-  // Parse navigation data //
-  ///////////////////////////
-  $referrer; $statesName;
+/////////////////////////
+// Parse user location //
+/////////////////////////
+$city = isset($json_message->userLocation->city) ? $json_message->userLocation->city : '[unknown]';
+$country = isset($json_message->userLocation->country) ? $json_message->userLocation->country : '[unknown]';
 
-  if (isset($obj->navigationHistory->referrer)) { $referrer = $obj->navigationHistory->referrer; } else { $referrer = '[unknown]'; };
-  if (isset($obj->navigationHistory->statesName) && is_array($obj->navigationHistory->statesName)) { $statesName = $obj->navigationHistory->statesName; } else { $statesName = array('[unknown]'); };
-
-  /////////////////////////
-  // Parse user location //
-  /////////////////////////
-  $city; $country;
-
-  if (isset($obj->userLocation->city)) { $city = $obj->userLocation->city; } else { $city = '[unknown]'; };
-  if (isset($obj->userLocation->country)) { $country = $obj->userLocation->country; } else { $country = '[unknown]'; };
-
-  //////////////////////
-  // Format the email //
-  //////////////////////
-  $emailBody = sprintf(
-    '<div style="color: #868d94; font-size: 13px;"><div><span style="color: #202a35; font-size: 20px; font-weight: bold;">%s</span><span> &#64; </span><span style="color: #2d3a3f; font-size: 16px;">%s</span></div><br><br><div>%s</div><br><div>Budget: <span>%s</span></div><br><br><div><a style="color: #868d94; font-size: 13px; font-weight: bold;">%s</a><span> &#62; </span><span>%s</span></div><div><a style="color: #868d94; font-size: 13px;">%s</a><span> &#45; </span><span>%s, %s</span></div></div>',
+//////////////////////
+// Format the email //
+//////////////////////
+$emailBody = sprintf(
+    '<div style="color: #868d94; font-size: 13px;">' .
+    '<div>' .
+    '<span style="color: #202a35; font-size: 20px; font-weight: bold;">%1$s</span>' .
+    '</div>' .
+    '<br><br>' .
+    '<div>%2$s</div>' .
+    '<br>' .
+    '<br><br>' .
+    '<div>' .
+    '<a style="color: #868d94; font-size: 13px; font-weight: bold;">%3$s</a>' . '<span> &#62; </span>' . '<span>%4$s</span>' .
+    '</div>' .
+    '<div>' .
+    '<a style="color: #868d94; font-size: 13px;">%5$s</a>' . '<span> &#45; </span>' . '<span>%6$s, %7$s</span>' .
+    '</div>' .
+    '</div>',
+    /* %1$s */
     $name,
-    $company,
+    /* %2$s */
     nl2br($help),
-    $budget,
+    /* %3$s */
     $referrer,
+    /* %4$s */
     implode(' / ', $statesName),
+    /* %5$s */
     $contactBack,
+    /* %6$s */
     $city,
+    /* %7$s */
     $country
-  );
+);
 
-  ////////////////////
-  // Send the email //
-  ////////////////////
-  function sendMail($subject, $body) {
+////////////////////
+// Send the email //
+////////////////////
+function sendMail($subject, $body)
+{
     $headers = "MIME-Version: 1.0\r\n";
     $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    // TODO add reply to original address
     $headers .= 'From: no-reply@hyperion.co<no-reply@hyperion.co>' . "\r\n" . 'Reply-To:no-reply@hyperion.co';
 
     mail('contact@hyperion.co', $subject, html_entity_decode($body), $headers);
     mail('cosmin@hyperion.co', $subject, html_entity_decode($body), $headers);
-  }
+}
 
-  sendMail('New Lead - ' . $name . ' from ' . $country, $emailBody);
-?>
+sendMail('New Lead - ' . $name . ' from ' . $country, $emailBody);
