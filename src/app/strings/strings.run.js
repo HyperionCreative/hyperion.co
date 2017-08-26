918b78b20c395591a956ed9b24f9a4361201a362
cozmy <cosmin@hyperion.co>
2017-08-26 14:13:20 +0300

    Changed the contact phone number

--- src/app/strings/strings.run.js
+++ src/app/strings/strings.run.js
@@ -7,8 +7,8 @@
       $rootScope.str = {
         globals: {
           personalPhoneNumber: '+44 7598-461-151',
-          desktopNumber: '+44 (0)1214-052-101',
-          phoneNumber: '+44 1214-052-101',
+          desktopNumber: '+44 (0)7598-461-151',
+          phoneNumber: '+44 7598-461-151',
           // This is not the address on which the contact form sends its emails.
           // That email address is defined in php/email-sender.php
           //
