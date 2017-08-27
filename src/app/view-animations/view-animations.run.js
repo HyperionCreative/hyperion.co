0edc161db4159c7c736fd942df75234966b6d5fc
cozmy <cosmin@hyperion.co>
2017-08-27 12:21:23 +0300

    Reordered (again) the main nav URLs

--- src/app/view-animations/view-animations.run.js
+++ src/app/view-animations/view-animations.run.js
@@ -16,9 +16,9 @@
         // 'root.sub-page-template.expertise.design',
         // 'root.sub-page-template.expertise.development',
 
+        'root.sub-page-template.portfolio',
         'root.sub-page-template.team',
         'root.sub-page-template.justification',
-        'root.sub-page-template.portfolio',
         'root.sub-page-template.contact'
       ];
       var PORTFOLIO_PROJECT_DETECTOR = 'root.sub-page-template.portfolio.';
